54709e53fb8f2e826c5d047a303fab8e0024f2f9
Siyan <liusiyan@users.noreply.github.com>
2024-04-07 12:52:08 -0400

    change matplotlib backend

--- pi3nn/Trainers/trainer_TS.py
+++ pi3nn/Trainers/trainer_TS.py
@@ -5,7 +5,8 @@ import pandas as pd
 import tensorflow as tf
 import os
 import matplotlib
-matplotlib.use("TkAgg")
+# matplotlib.use("TkAgg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import time
 import math
